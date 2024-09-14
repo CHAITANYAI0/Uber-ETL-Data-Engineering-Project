7478fa21e2da29438666785445a61318da175f8a
Chaitanya Inamdar <susmiti63@gmail.com>
2024-09-13 22:29:10 -0400

    Update analytics_query.sql

--- analytics_query.sql
+++ analytics_query.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE TABLE `data-with-darshil.uber_data_engineering_yt.tbl_analytics` AS (
+CREATE OR REPLACE TABLE `data-with-chaitanya.uber_data_engineering_.tbl_analytics` AS (
 SELECT 
 f.trip_id,
 f.VendorID,
@@ -21,12 +21,12 @@ f.improvement_surcharge,
 f.total_amount
 FROM 
 
-`data-with-darshil.uber_data_engineering_yt.fact_table` f
-JOIN `data-with-darshil.uber_data_engineering_yt.datetime_dim` d  ON f.datetime_id=d.datetime_id
-JOIN `data-with-darshil.uber_data_engineering_yt.passenger_count_dim` p  ON p.passenger_count_id=f.passenger_count_id  
-JOIN `data-with-darshil.uber_data_engineering_yt.trip_distance_dim` t  ON t.trip_distance_id=f.trip_distance_id  
-JOIN `data-with-darshil.uber_data_engineering_yt.rate_code_dim` r ON r.rate_code_id=f.rate_code_id  
-JOIN `data-with-darshil.uber_data_engineering_yt.pickup_location_dim` pick ON pick.pickup_location_id=f.pickup_location_id
-JOIN `data-with-darshil.uber_data_engineering_yt.dropoff_location_dim` drop ON drop.dropoff_location_id=f.dropoff_location_id
-JOIN `data-with-darshil.uber_data_engineering_yt.payment_type_dim` pay ON pay.payment_type_id=f.payment_type_id)
+`data-with-darshil.uber_data_engineering_.fact_table` f
+JOIN `data-with-chaitanya.uber_data_engineering_.datetime_dim` d  ON f.datetime_id=d.datetime_id
+JOIN `data-with-chaitanya.uber_data_engineering_.passenger_count_dim` p  ON p.passenger_count_id=f.passenger_count_id  
+JOIN `data-with-chaitanya.uber_data_engineering_.trip_distance_dim` t  ON t.trip_distance_id=f.trip_distance_id  
+JOIN `data-with-chaitanya.uber_data_engineering_.rate_code_dim` r ON r.rate_code_id=f.rate_code_id  
+JOIN `data-with-chaitanya.uber_data_engineering_.pickup_location_dim` pick ON pick.pickup_location_id=f.pickup_location_id
+JOIN `data-with-chaitanya.uber_data_engineering_.dropoff_location_dim` drop ON drop.dropoff_location_id=f.dropoff_location_id
+JOIN `data-with-chaitanya.uber_data_engineering_.payment_type_dim` pay ON pay.payment_type_id=f.payment_type_id)
 ;
